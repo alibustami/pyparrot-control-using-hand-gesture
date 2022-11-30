bed4165195537dbaf3242ac673ef3ca952efc01a
Ali Albustami <aalbustami@beyond.ai>
2022-12-01 01:51:14 +0300

    build the infrastructure to the FingersCounter

---
+++ src/FingersCounter.py
@@ -0,0 +1,10 @@
+"""This module contains the FingersCounter class"""
+from typing import Any
+
+
+class FingersCounter:
+    def __init__(self):
+        pass
+
+    def count_from_image(self, image: Any):
+        pass
