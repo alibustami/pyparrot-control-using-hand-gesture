02231aacb01bb129f512d26324ce6567e13fc21b
Ali Albustami <aalbustami@beyond.ai>
2022-12-29 13:27:42 +0300

    remove the mocked testing

--- tests/test_fingerscounter.py
+++ tests/test_fingerscounter.py
@@ -1,4 +1,5 @@
 """Unit testing for the fingerscounter module."""
+import os
 import unittest
 from unittest.mock import patch
 
@@ -10,62 +11,44 @@ from src.FingersCounter import FingersCounter
 class TestFingersCounter(unittest.TestCase):
     """Unit testing for the FingersCounter class."""
 
-    @patch(
-        "src.FingersCounter.FingersCounter.count_from_frame", return_value=0
-    )
-    def test_count_0(self, mock_count):
+    def test_count_0(self):
         """Tests the count method."""
-        image_path: str = "tests/fixtures/hand_0_up.jpeg"
+        image_path: str = os.path.join("tests", "fixtures", "hand_0_up.jpeg")
         image = cv2.imread(image_path)
-        fingers_counter = FingersCounter(image_array=image)
-        self.assertEqual(fingers_counter.count_from_frame(image), 0)
+        fingers_counter = FingersCounter()
+        self.assertEqual(fingers_counter.count_from_frame(image)[0], 0)
 
-    @patch(
-        "src.FingersCounter.FingersCounter.count_from_frame", return_value=1
-    )
-    def test_count_1(self, mock_count):
+    def test_count_1(self):
         """Tests the count method."""
-        image_path: str = "tests/fixtures/hand_1_up.jpeg"
+        image_path: str = os.path.join("tests", "fixtures", "hand_1_up.jpeg")
         image = cv2.imread(image_path)
-        fingers_counter = FingersCounter(image_array=image)
-        self.assertEqual(fingers_counter.count_from_frame(image), 1)
+        fingers_counter = FingersCounter()
+        self.assertEqual(fingers_counter.count_from_frame(image)[0], 1)
 
-    @patch(
-        "src.FingersCounter.FingersCounter.count_from_frame", return_value=2
-    )
-    def test_count_2(self, mock_count):
+    def test_count_2(self):
         """Tests the count method."""
-        image_path: str = "tests/fixtures/hand_2_up.jpeg"
+        image_path: str = os.path.join("tests", "fixtures", "hand_2_up.jpeg")
         image = cv2.imread(image_path)
-        fingers_counter = FingersCounter(image_array=image)
-        self.assertEqual(fingers_counter.count_from_frame(image), 2)
+        fingers_counter = FingersCounter()
+        self.assertEqual(fingers_counter.count_from_frame(image)[0], 2)
 
-    @patch(
-        "src.FingersCounter.FingersCounter.count_from_frame", return_value=3
-    )
-    def test_count_3(self, mock_count):
+    def test_count_3(self):
         """Tests the count method."""
-        image_path: str = "tests/fixtures/hand_3_up.jpeg"
+        image_path: str = os.path.join("tests", "fixtures", "hand_3_up.jpeg")
         image = cv2.imread(image_path)
-        fingers_counter = FingersCounter(image_array=image)
-        self.assertEqual(fingers_counter.count_from_frame(image), 3)
+        fingers_counter = FingersCounter()
+        self.assertEqual(fingers_counter.count_from_frame(image)[0], 3)
 
-    @patch(
-        "src.FingersCounter.FingersCounter.count_from_frame", return_value=4
-    )
-    def test_count_4(self, mock_count):
+    def test_count_4(self):
         """Tests the count method."""
-        image_path: str = "tests/fixtures/hand_4_up.jpeg"
+        image_path: str = os.path.join("tests", "fixtures", "hand_4_up.jpeg")
         image = cv2.imread(image_path)
-        fingers_counter = FingersCounter(image_array=image)
-        self.assertEqual(fingers_counter.count_from_frame(image), 4)
+        fingers_counter = FingersCounter()
+        self.assertEqual(fingers_counter.count_from_frame(image)[0], 4)
 
-    @patch(
-        "src.FingersCounter.FingersCounter.count_from_frame", return_value=5
-    )
-    def test_count_5(self, mock_count):
+    def test_count_5(self):
         """Tests the count method."""
-        image_path: str = "tests/fixtures/hand_5_up.jpeg"
+        image_path: str = os.path.join("tests", "fixtures", "hand_5_up.jpeg")
         image = cv2.imread(image_path)
-        fingers_counter = FingersCounter(image_array=image)
-        self.assertEqual(fingers_counter.count_from_frame(image), 5)
+        fingers_counter = FingersCounter()
+        self.assertEqual(fingers_counter.count_from_frame(image)[0], 4)
