58053bd28cb98600647cc9f17323441b5b6dc500
Ali Albustami <aalbustami@beyond.ai>
2023-01-02 15:52:25 +0300

    add shell script

---
+++ build.sh
@@ -0,0 +1,6 @@
+conda env create -f environment.yaml
+conda activate parrot-env
+python -m pip install --upgrade pip
+pip install --upgrade pip setuptools wheel
+python -m pip install -e .
+pre-commit install
